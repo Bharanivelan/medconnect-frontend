1399cc0a3924c62fa2ed5f68c6e750781fcc4962
bharanivelan <bharani01072005@gmail.com>
2025-03-12 23:08:53 +0530

    api

--- src/api/api.ts
+++ src/api/api.ts
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL for backend
 const API = axios.create({
-  baseURL: "http://localhost:5001/api",
+  baseURL: "https://medconnect-backend-beta.vercel.app/",
 });
 
 // Add token for protected routes (if needed)
